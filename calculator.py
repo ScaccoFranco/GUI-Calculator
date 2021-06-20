046b73553f255b691c18f9dfc72d12793abc1abf
ScaccoFranco <samuelefranco@gmail.com>
2021-06-20 22:31:04 +0200

    operations with priority to divisions and multiplications

--- calculator.py
+++ calculator.py
@@ -26,34 +26,34 @@ class GUI:
 
 
         # Set the numbers from 0 to 9 buttons
-        button1 = Button(self.window, text="1", font=text_style, width=5, height=3, command=lambda: self.add(1))
+        button1 = Button(self.window, text="1", font=text_style, width=5, height=3, command=lambda: self.add_number(1))
         button1.grid(row=2, column=0, sticky=N + S + E + W)
 
-        button2 = Button(self.window, text="2", font=text_style, width=5, height=3, command=lambda: self.add(2))
+        button2 = Button(self.window, text="2", font=text_style, width=5, height=3, command=lambda: self.add_number(2))
         button2.grid(row=2, column=1, sticky=N + S + E + W)
 
-        button3 = Button(self.window, text="3", font=text_style, width=5, height=3, command=lambda: self.add(3))
+        button3 = Button(self.window, text="3", font=text_style, width=5, height=3, command=lambda: self.add_number(3))
         button3.grid(row=2, column=2, sticky=N + S + E + W)
 
-        button4 = Button(self.window, text="4", font=text_style, width=5, height=3, command=lambda: self.add(4))
+        button4 = Button(self.window, text="4", font=text_style, width=5, height=3, command=lambda: self.add_number(4))
         button4.grid(row=3, column=0, sticky=N + S + E + W)
 
-        button5 = Button(self.window, text="5", font=text_style, width=5, height=3, command=lambda: self.add(5))
+        button5 = Button(self.window, text="5", font=text_style, width=5, height=3, command=lambda: self.add_number(5))
         button5.grid(row=3, column=1, sticky=N + S + E + W)
 
-        button6 = Button(self.window, text="6", font=text_style, width=5, height=3, command=lambda: self.add(6))
+        button6 = Button(self.window, text="6", font=text_style, width=5, height=3, command=lambda: self.add_number(6))
         button6.grid(row=3, column=2, sticky=N + S + E + W)
 
-        button7 = Button(self.window, text="7", font=text_style, width=5, height=3, command=lambda: self.add(7))
+        button7 = Button(self.window, text="7", font=text_style, width=5, height=3, command=lambda: self.add_number(7))
         button7.grid(row=4, column=0, sticky=N + S + E + W)
 
-        button8 = Button(self.window, text="8", font=text_style, width=5, height=3, command=lambda: self.add(8))
+        button8 = Button(self.window, text="8", font=text_style, width=5, height=3, command=lambda: self.add_number(8))
         button8.grid(row=4, column=1, sticky=N + S + E + W)
 
-        button9 = Button(self.window, text="9", font=text_style, height=3, width=5, command=lambda: self.add(9))
+        button9 = Button(self.window, text="9", font=text_style, height=3, width=5, command=lambda: self.add_number(9))
         button9.grid(row=4, column=2, sticky=N + S + E + W)
 
-        button10 = Button(self.window, text="0", font=text_style, height=3, width=5, command=lambda: self.add(0))
+        button10 = Button(self.window, text="0", font=text_style, height=3, width=5, command=lambda: self.add_number(0))
         button10.grid(row=5, column=0, sticky=N + S + E + W)
 
         # Set the CANC button
@@ -61,16 +61,16 @@ class GUI:
         button11.grid(row=2, column=3, sticky=N + S + E + W)
 
         # Set the operators + - * / buttons
-        button12 = Button(self.window, text="+", font=text_style, height=3, width=5, command=lambda: self.add('+'))
+        button12 = Button(self.window, text="+", font=text_style, height=3, width=5, command=lambda: self.add_operator('+'))
         button12.grid(row=3, column=3, sticky=N + S + E + W)
 
-        button13 = Button(self.window, text="x", font=text_style, height=3, width=5, command=lambda: self.add('x'))
+        button13 = Button(self.window, text="x", font=text_style, height=3, width=5, command=lambda: self.add_operator('x'))
         button13.grid(row=4, column=3, sticky=N + S + E + W)
 
-        button14 = Button(self.window, text="/", font=text_style, height=3, width=5, command=lambda: self.add('/'))
+        button14 = Button(self.window, text="/", font=text_style, height=3, width=5, command=lambda: self.add_operator('/'))
         button14.grid(row=5, column=3, sticky=N + S + E + W)
 
-        button15 = Button(self.window, text="-", font=text_style, height=3, width=5, command=lambda: self.add('-'))
+        button15 = Button(self.window, text="-", font=text_style, height=3, width=5, command=lambda: self.add_operator('-'))
         button15.grid(row=5, column=2, sticky=N + S + E + W)
 
         # Set the result button 
@@ -78,129 +78,127 @@ class GUI:
         button16.grid(row=5, column=1, sticky=N + S + E + W)
 
 
-    def add(self, value):
-
+    def add_number(self, value):
         # This function is called when I press a number or operator button
         
-        # Control the value of the variables and the value of the button pressed
-        # In case of syntax error call the self.error() function 
-        if self.first == "":
+        # Control the place to store the value and save it there 
+        # Then print the value
+        if self.tmp_number == None:
+            self.tmp_number = value
+            self.print_output(self.tmp_number)
+        
+        elif self.tmp_operator != 'x' and self.tmp_operator != '/':
+            self.tmp_number = self.tmp_number * 10 + value
+            self.print_output(self.tmp_number)
 
-            if type(value) == int:
-                self.first = value
-            
-            else:
-                self.error()
+        elif self.tmp_number2 == None:
+            self.tmp_number2 = value
+            self.print_output(self.tmp_number2)
 
-        elif self.operator == "":
+        else:
+            self.tmp_number2 = self.tmp_number2 * 10 + value
+            self.print_output(self.tmp_number2)
 
-            if type(value) == int:
-                self.first = self.first * 10 + value
 
-            else:
-                self.operator = value
+    def add_operator(self, value):
+        # This function get called when the user press an operator button
 
-        elif self.second == "":
+        # Check if the next operation is a multiplication or not
+        is_mult = (value == 'x' or value == '/')
 
-            if type(value) == int:
-                self.second = value
+        # Calculate the last operation
+        self.calculate(is_multiplication=is_mult)
 
-            else:
-                self.error()
 
-        else: 
+        self.tmp_operator = value
 
-            if type(value) == int:
-                self.second = self.second * 10 + value
+    
+    def calculate(self, is_multiplication=False):
+        # Check if this is there are syntax errors
+        # in case print a warning in the output
+        if self.tmp_number == None:
+            self.error()  
+            return
 
-            else:
-                self.first = self.calculate()
-                self.operator = value
-                self.second = ""
+        if self.tmp_operator != None and self.tmp_number == None:
+            self.error()
+            return
 
-        self.print_output()
+        if (self.tmp_operator == 'x' or self.tmp_operator == '/') and self.tmp_number2 == None:
+            self.error()
+            return
+
+        if self.tmp_operator == '/' and self.tmp_number2 == 0:
+            self.error()
+            return
+
+        # Calculate the result, doing at first multiplications and divisions,
+        # than additions and subtractions
+        if self.tmp_operator == '-':
+            self.tmp_number *= -1
+
+        elif self.tmp_operator == 'x':
+            self.tmp_number *= self.tmp_number2 
+            self.tmp_numer2 = None
+
+        elif self.tmp_operator == '/':
+            self.tmp_number *= (self.tmp_number2) ** -1
+            self.tmp_numer2 = None
+
+        
+        # If the next operation is not an operation or a division 
+        # add the result of this operation to the final one
+        if not is_multiplication:
+            self.result_number += self.tmp_number
+            self.tmp_number = None
 
 
     def canc(self, print=True):
         # This function is called to delete (or initalize) the variables of the calculator
 
-        # Set the variable to "" for don't show them
-        self.first = ""
-        self.operator = ""
-        self.second = ""
-        self.third = ""
+        # Set the variable to None 
+        self.result_number = 0
+        self.tmp_number = None
+        self.tmp_number2 = None         # This variable is used only for moltiplications and divisions
+        self.tmp_operator = None         
 
         # After calculating the result whe delete the variables,
         # but we keep the old output setting the variable 'print' to False 
         if print:
-            self.print_output()
+            self.print_output(" ")
 
 
     def result(self):
         # This function is called for calculate the result and print it in the output widget
 
-        # Assign at the result variable the result
-        self.third = self.calculate()
-
-        # if there is a syntax error print 'Syntax Error' in the output widget
-        if self.third == None:
-            self.error()
-
+        # Calculate the last operations 
+        self.calculate()
+        
         # Print the output in the output widget
-        self.print_output()
+        if self.result_number == None:
+            self.print_output(0)
+
+        else:
+            self.print_output(self.result_number)
 
         # Delete the variables but keep showing them
-        self.canc(False)
-        
-
-    def calculate(self):
-        # This function is called to return the operation result value
-
-        # If there are no values in the operaton it returns 0
-        if self.first == "":
-            return 0
-
-        # If the operation is composed only by a number the function returns it
-        elif self.operator == "":
-            return self.first
-        
-        # If the operation is composed only by a number and an operator 
-        # return None, so in the result function call the self.error() function
-        elif self.second == "":
-            return None
-
-        # Else if there are 2 numbers and an operator
-        # it controls the operator and return the right result
-        else:
-
-            if self.operator == '+':
-                return self.first + self.second
-
-            elif self.operator == '-':
-                return self.first - self.second
-
-            elif self.operator == 'x':
-                return self.first * self.second
-
-            elif self.operator == '/':
-                return self.first / float(self.second)
+        self.canc(print=False)
 
     
-    def print_output(self):
+    def print_output(self, val):
         # This function prints the output in the output widget
 
-        # It prints the 2 numbers and the operator in the first line and the result in the second one
-        self.output.set(f"{self.first} {self.operator} {self.second}\n{self.third}")
+        # Print the argument passed in this function
+        # It's always the result_variable or the tmp_number variable
+        self.output.set(f"\n{val}\n")
 
 
     def error(self):
         # This function is called to print 'Syntax Error' in the output widget
 
-        # It deletes the variables and set the first to the string 'Error!', so it's shown in the output
-        self.first = "Syntax Error"
-        self.operator = ''
-        self.second = ''
-        self.third = ''
+        # Delete the variables and print string 'Error!'
+        self.canc(print=False)
+        self.print_output("Syntax Error!")
 
 
 if __name__ == '__main__':
